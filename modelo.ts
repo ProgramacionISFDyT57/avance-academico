5f370d853faaaf0b5a14034494919107e90a9088
Fernando Cortese <cortesefernando@gmail.com>
2018-10-11 20:57:03 -0300

    crear cursada

--- modelo.ts
+++ modelo.ts
@@ -29,3 +29,11 @@ export class InscripcionCarrera {
     id_carrera_abierta: number;
     fecha: string;
 }
+export class Cursada {
+    id: number;
+    id_materia: number;
+    id_profesor: number;
+    año: number;
+    fecha_inicio: string;
+    fecha_limite: string;
+}
