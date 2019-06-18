29ff57ac619916ed0b17ae38561a84cbffd858e6
Fernando Cortese <cortesefernando@gmail.com>
2019-06-18 18:19:22 -0300

    fix consulta ver materias

--- controllers/materias-controller.ts
+++ controllers/materias-controller.ts
@@ -94,7 +94,7 @@ export class MateriasController {
             INNER JOIN carreras c ON c.id = m.id_carrera
             INNER JOIN correlativas co ON co.id_materia = m.id
             INNER JOIN materias mc ON mc.id = co.id_correlativa
-            GROUP BY m.id, m.nombre, m.anio, tm.nombre AS tipo_materia, c.nombre AS carrera
+            GROUP BY m.id, m.nombre, m.anio, tm.nombre, c.nombre
             ORDER BY c.nombre, m.anio, m.nombre`;
         this.db.manyOrNone(query)
             .then((data) => {
