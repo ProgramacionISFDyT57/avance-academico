9b3f1313f37c32ed196972faabf3b60251cf9d65
Fernando Cortese <cortesefernando@gmail.com>
2019-08-13 20:03:35 -0300

    fix

--- controllers/mesas-controller.ts
+++ controllers/mesas-controller.ts
@@ -30,7 +30,7 @@ export class MesasController {
     public lista_mesas(req: Request, res: Response) {
         const query = `
             SELECT me.id, ma.nombre AS materia, ma.anio AS anio_materia, me.fecha_inicio, me.fecha_limite, 
-                me.fecha_examen, ca.nombre AS carrera
+                me.fecha_examen, ca.nombre AS carrera,
                 CONCAT_WS(', ', us.apellido, us.nombre) AS profesor,
                 CONCAT_WS(', ', us1.apellido, us1.nombre) AS vocal1,
                 CONCAT_WS(', ', us2.apellido, us2.nombre) AS vocal2,
