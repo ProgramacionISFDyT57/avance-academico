95b8db21a4802274b3d5c55066e16ab4c4e0553f
Fernando Cortese <cortesefernando@gmail.com>
2019-09-11 11:48:11 -0300

    devuelve id correlativas

--- controllers/materias-controller.ts
+++ controllers/materias-controller.ts
@@ -91,7 +91,7 @@ export class MateriasController {
     public ver_materias(req: Request, res: Response) {
         const query = `
             SELECT m.id, m.nombre, m.anio, tm.nombre AS tipo_materia, tm.id AS id_tipo_materia, c.nombre AS carrera, c.id AS id_carrera, c.duracion AS duracion_carrera,
-                cu.ultima_cursada, array_agg(mc.nombre) AS correlativas
+                cu.ultima_cursada, array_agg(mc.nombre) AS correlativas, array_agg(mc.id) AS id_correlativas
             FROM materias m
             INNER JOIN tipos_materias tm ON tm.id = m.id_tipo
             INNER JOIN carreras c ON c.id = m.id_carrera
